e0663cece02f04ee61718db82951287b473812a3
OkazakiRui <okazaki200226@gmail.com>
2022-07-26 21:57:59 +0900

    feat: google認証のみにした

--- features/auth/components/AuthForm.tsx
+++ features/auth/components/AuthForm.tsx
@@ -1,72 +1,10 @@
 import { FC } from 'react'
-import NextLink from 'next/link'
 
 const AuthForm: FC = () => (
   <div className="w-full max-w-sm p-6 m-auto bg-white rounded-md shadow-md dark:bg-gray-800">
     <h1 className="text-3xl font-semibold text-center text-gray-700 dark:text-white">
-      Brand
+      Authentication
     </h1>
-
-    <form className="mt-6">
-      <div>
-        <label
-          htmlFor="username"
-          className="block text-sm text-gray-800 dark:text-gray-200"
-        >
-          Username
-        </label>
-        <input
-          type="text"
-          id="username"
-          className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
-        />
-      </div>
-
-      <div className="mt-4">
-        <div className="flex items-center justify-between">
-          <label
-            htmlFor="password"
-            className="block text-sm text-gray-800 dark:text-gray-200"
-          >
-            Password
-          </label>
-          <NextLink
-            href="#"
-            className="text-xs text-gray-600 dark:text-gray-400 hover:underline"
-          >
-            Forget Password?
-          </NextLink>
-        </div>
-
-        <input
-          type="password"
-          id="password"
-          className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
-        />
-      </div>
-
-      <div className="mt-6">
-        <button
-          type="button"
-          className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
-        >
-          Login
-        </button>
-      </div>
-    </form>
-
-    <div className="flex items-center justify-between mt-4">
-      <span className="w-1/5 border-b dark:border-gray-600 lg:w-1/5" />
-
-      <NextLink
-        href="#"
-        className="text-xs text-center text-gray-500 uppercase dark:text-gray-400 hover:underline"
-      >
-        or login with Social Media
-      </NextLink>
-
-      <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/5" />
-    </div>
 
     <div className="flex items-center mt-6 -mx-2">
       <button
@@ -79,25 +17,18 @@ const AuthForm: FC = () => (
 
         <span className="hidden mx-2 sm:inline">Sign in with Google</span>
       </button>
-
-      <NextLink
-        href="#"
-        className="p-2 mx-2 text-sm font-medium text-gray-500 transition-colors duration-200 transform bg-gray-300 rounded-md hover:bg-gray-200"
-      >
-        <svg className="w-5 h-5 fill-current" viewBox="0 0 24 24">
-          <path d="M23.954 4.569c-.885.389-1.83.654-2.825.775 1.014-.611 1.794-1.574 2.163-2.723-.951.555-2.005.959-3.127 1.184-.896-.959-2.173-1.559-3.591-1.559-2.717 0-4.92 2.203-4.92 4.917 0 .39.045.765.127 1.124C7.691 8.094 4.066 6.13 1.64 3.161c-.427.722-.666 1.561-.666 2.475 0 1.71.87 3.213 2.188 4.096-.807-.026-1.566-.248-2.228-.616v.061c0 2.385 1.693 4.374 3.946 4.827-.413.111-.849.171-1.296.171-.314 0-.615-.03-.916-.086.631 1.953 2.445 3.377 4.604 3.417-1.68 1.319-3.809 2.105-6.102 2.105-.39 0-.779-.023-1.17-.067 2.189 1.394 4.768 2.209 7.557 2.209 9.054 0 13.999-7.496 13.999-13.986 0-.209 0-.42-.015-.63.961-.689 1.8-1.56 2.46-2.548l-.047-.02z" />
-        </svg>
-      </NextLink>
     </div>
 
     <p className="mt-8 text-xs font-light text-center text-gray-400">
       Don&apos;t have an account?
-      <NextLink
-        href="#"
+      <a
+        target="_blank"
+        href="https://accounts.google.com/signup/v2/webcreateaccount?continue=https%3A%2F%2Faccounts.google.com%2FManageAccount%3Fnc%3D1&dsh=S-1543265819%3A1658840018083227&biz=false&flowName=GlifWebSignIn&flowEntry=SignUp"
         className="font-medium text-gray-700 dark:text-gray-200 hover:underline"
+        rel="noreferrer"
       >
         Create One
-      </NextLink>
+      </a>
     </p>
   </div>
 )
