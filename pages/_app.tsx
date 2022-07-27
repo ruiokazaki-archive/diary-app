fe3970895f127d8d6ec74a829249aba264eb0f7c
OkazakiRui <okazaki200226@gmail.com>
2022-07-27 20:06:25 +0900

    feat: create auth context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AuthProvider } from '../features/auth/store/AuthContext'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  <Component {...pageProps} />
+  <AuthProvider>
+    {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+    <Component {...pageProps} />
+  </AuthProvider>
 )
 
 export default MyApp
