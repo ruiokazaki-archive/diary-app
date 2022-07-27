e62c12d8ef069e5208b40faf0248455568b51a35
OkazakiRui <okazaki200226@gmail.com>
2022-07-28 00:38:11 +0900

    feat: `WIP`投稿フォームの一部作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,100 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <h1 className="text-xl">hello world!</h1>
+  'a'
+
+  return (
+    <div className="flex flex-col items-center min-h-screen pt-6 bg-gray-100 sm:justify-center sm:pt-0">
+      <div className="w-full px-16 py-20 mt-6 overflow-hidden bg-white rounded-lg lg:max-w-4xl">
+        <h1 className="mb-4 text-3xl font-bold decoration-gray-400">
+          投稿する
+        </h1>
+
+        <div className="w-full px-6 py-4 bg-white rounded shadow-md ring-1 ring-gray-900/10">
+          <form className="flex flex-col gap-2">
+            {/* title */}
+            <div>
+              <label
+                htmlFor="title"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                タイトル
+              </label>
+              <div className="flex">
+                <select className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-2 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 dark:border-gray-700 dark:text-white rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                  <option value={1}>😖</option>
+                  <option value={2}>😔</option>
+                  <option value={3} selected>
+                    🙂
+                  </option>
+                  <option value={4}>😚</option>
+                  <option value={5}>🥳</option>
+                  <option disabled>---</option>
+                  <option value={6}>😡</option>
+                  <option value={7}>😭</option>
+                  <option disabled>---</option>
+                  <option value={8}>✍️</option>
+                </select>
+                <div className="relative w-full">
+                  <input
+                    id="title"
+                    className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-100 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+                    placeholder="タイトルを入力してください"
+                    required
+                  />
+                </div>
+              </div>
+            </div>
+
+            {/* Richtext */}
+            <div />
+
+            {/* dropzone */}
+            <div>
+              <label
+                htmlFor="dropzone-file"
+                className="flex flex-col justify-center items-center w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+              >
+                <div className="flex flex-col justify-center items-center pt-5 pb-6">
+                  <svg
+                    aria-hidden="true"
+                    className="mb-3 w-10 h-10 text-gray-400"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                    />
+                  </svg>
+                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                    <span className="font-semibold">Click to upload </span>
+                    or drag and drop
+                  </p>
+                  <p className="text-xs text-gray-500 dark:text-gray-400">
+                    SVG, PNG, JPG or GIF
+                  </p>
+                </div>
+                <input id="dropzone-file" type="file" className="hidden" />
+              </label>
+            </div>
+
+            {/* submit button */}
+            <button
+              type="submit"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              投稿する
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
