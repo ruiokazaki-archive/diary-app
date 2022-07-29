8233384d131e5312ed63621e0af99e7c9b51b661
OkazakiRui <okazaki200226@gmail.com>
2022-07-29 12:27:27 +0900

    feat: configを適用

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { IconContext } from 'react-icons'
 import { AuthProvider } from '../features/auth/store/AuthContext'
+import config from '../libs/react-icons'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <AuthProvider>
-    {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-    <Component {...pageProps} />
-  </AuthProvider>
+  <IconContext.Provider value={config}>
+    <AuthProvider>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+    </AuthProvider>
+  </IconContext.Provider>
 )
 
 export default MyApp
