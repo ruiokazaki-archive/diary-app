78f9bb5276bd77880385a28e4edb2c4e3f957298
OkazakiRui <okazaki200226@gmail.com>
2022-07-31 14:04:02 +0900

    feat: add ToastContainer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,25 @@
 import '../styles/globals.css'
+import 'react-toastify/dist/ReactToastify.css'
 import type { AppProps } from 'next/app'
 import { IconContext } from 'react-icons'
+import { ToastContainer } from 'react-toastify'
 import { AuthProvider } from '../features/auth/store/AuthContext'
-import config from '../libs/react-icons'
+import iconConfig from '../libs/react-icons'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <IconContext.Provider value={config}>
+  <IconContext.Provider value={iconConfig}>
     <AuthProvider>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <Component {...pageProps} />
     </AuthProvider>
